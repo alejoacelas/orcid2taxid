333650b6a50f24923b8f87884f7fa8e91db65756
AlejoAcelas <alejoacelas@gmail.com>
2025-03-02 19:43:31 -0500

    change repository interface

--- src/orcid2taxid/data/repositories/base_repository.py
+++ src/orcid2taxid/data/repositories/base_repository.py
@@ -11,7 +11,7 @@ class BasePublicationRepository(ABC):
     SUPPORTS_ORCID_SEARCH: bool = False
 
     @abstractmethod
-    def search_by_orcid(self, orcid: str, max_results: int = 20) -> List[PaperMetadata]:
+    def get_publications_by_orcid(self, orcid: str, max_results: int = 20) -> List[PaperMetadata]:
         """
         Search repository by ORCID ID.
         :param orcid: ORCID ID.
@@ -21,7 +21,7 @@ class BasePublicationRepository(ABC):
         pass
 
     @abstractmethod
-    def search_by_author_metadata(self, author_metadata: AuthorMetadata, max_results: int = 20) -> List[PaperMetadata]:
+    def get_publications_by_author_metadata(self, author_metadata: AuthorMetadata, max_results: int = 20) -> List[PaperMetadata]:
         """
         Search repository by author metadata.
         :param author_metadata: Author metadata dictionary.
@@ -50,7 +50,7 @@ class BasePublicationRepository(ABC):
         """
         pass
 
-    def search_publications(self, orcid: str, author_metadata: AuthorMetadata, max_results: int = 20) -> List[PaperMetadata]:
+    def get_publications(self, orcid: str, author_metadata: AuthorMetadata, max_results: int = 20) -> List[PaperMetadata]:
         """
         Smart search that uses the most appropriate search method based on repository capabilities.
         
@@ -60,5 +60,5 @@ class BasePublicationRepository(ABC):
         :return: List of publication metadata dictionaries.
         """
         if self.SUPPORTS_ORCID_SEARCH:
-            return self.search_by_orcid(orcid, max_results)
-        return self.search_by_author_metadata(author_metadata, max_results)
+            return self.get_publications_by_orcid(orcid, max_results)
+        return self.get_publications_by_author_metadata(author_metadata, max_results)
